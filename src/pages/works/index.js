d912c819b41e67d8bd200cb5c5a45421d6478726
clemente <elicran.on@gmail.com>
2021-08-25 15:50:14 +0900

    move background to layout, add new pages for works

---
+++ src/pages/works/index.js
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+import Layout from '../../components/layout'
+
+
+const Works = ({ data }) => {
+  return (
+    <Layout>
+    <div className={style.container}>
+    {data.allMdx.nodes.map((item)=> {
+      return (
+        <div>
+          <h2><span>{item.frontmatter.title}</span></h2>
+        </div>
+      );
+    })}
+    </div>
+    </Layout>
+  )
+}
+
+export default Works
+
+export const query = graphql`
+  query {
+    allMdx {
+      nodes {
+        slug
+        frontmatter {
+          title
+          category
+          tool
+        }
+      }
+    }
+  }
+`
