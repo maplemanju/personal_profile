981f9b6fc1af4151334204f408913855b8f02e3e
clemente <elicran.on@gmail.com>
2021-08-31 16:51:52 +0900

    change icons directory to favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,49 @@ module.exports = {
         name: "Amayadori Personal Profile",
         short_name: "Amayadori Profile",
         start_url: "/",
-        icon: "src/images/icon.png", 
+        icon: "src/images/icon.png",
+        icons: [
+          {
+            "src": "favicons/icon-48x48.png",
+            "sizes": "48x48",
+            "type": "image/png"
+          },
+          {
+            "src": "favicons/icon-72x72.png",
+            "sizes": "72x72",
+            "type": "image/png"
+          },
+          {
+            "src": "favicons/icon-96x96.png",
+            "sizes": "96x96",
+            "type": "image/png"
+          },
+          {
+            "src": "favicons/icon-144x144.png",
+            "sizes": "144x144",
+            "type": "image/png"
+          },
+          {
+            "src": "favicons/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "favicons/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+          },
+          {
+            "src": "favicons/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+            "src": "favicons/icon-512x512.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+        ],
       },
     },
     {
